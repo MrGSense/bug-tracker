940b1866f207aae9e9273a1f08983904a9099043
Kyle <kyle.fetterhoff@gmail.com>
2019-12-16 14:23:42 -0500

    Add start to refactoring back-end

--- models/Bug.js
+++ models/Bug.js
@@ -1,25 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BugSchema = new Schema({
-    bug_title: {
-        type: String,
-        required: true
-    },
-    bug_project: {
-        type: String,
-        required: true
-    },
-    bug_description: {
-        type: String,
-        required: true
-    },
-    solved: false,
-    comments: [],
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  bug_title: {
+    type: String,
+    required: true
+  },
+  bug_project: {
+    type: String,
+    required: true
+  },
+  bug_description: {
+    type: String,
+    required: true
+  },
+  solved: false,
+  comments: [],
+  githubRepo: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = Bug = mongoose.model('bug', BugSchema);
+module.exports = Bug = mongoose.model("bug", BugSchema);
--- routes/api/bugs.js
+++ routes/api/bugs.js
@@ -1,37 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth');
+const auth = require("../../middleware/auth");
 
 // User Model
-const Bug = require('../../models/Bug');
+const Bug = require("../../models/Bug");
 
 // @route GET api/bugs
 // @desc Get bugs
 // @access Public
-router.get('/', (req,res) => {
-    Bug.find()
-    .then(bugs => res.json(bugs))
+router.get("/", (req, res) => {
+  Bug.find().then(bugs => res.json(bugs));
 });
 
 // @route POST api/bugs
 // @desc Create new bug
 // @access Private
-router.post('/', auth, (req, res) => {
-    const newBug = new Bug({
-        bug_title: req.body.title,
-        bug_project: req.body.project,
-        bug_description: req.body.description
-    });
-    newBug.save().then(bug => res.json(bug));
+router.post("/", auth, (req, res) => {
+  const newBug = new Bug({
+    bug_title: req.body.title,
+    bug_project: req.body.project,
+    bug_description: req.body.description
+  });
+
+  // @todo make it the ability to create a new or edit a bug
+  newBug.save().then(bug => res.json(bug));
 });
 
 // @route DELETE api/bugs/:id
 // @desc Delete bug
 // @access Private
-router.delete('/:id', auth, (req, res) => {
-    Bug.findById(req.params.id)
+router.delete("/:id", auth, (req, res) => {
+  Bug.findById(req.params.id)
     .then(bug => bug.remove().then(() => res.json({ success: true })))
     .catch(err => res.status(404).json({ success: false }));
+});
+
+// @route PUT api/bugs/:id
+// @desc Add comment to bug
+// @access Private
+router.put("/:id", auth, (req, res) => {
+  // @todo add mongoose stuff to be able to add a comment
 });
 
 module.exports = router;
