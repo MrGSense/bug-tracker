0e230974b1a777cef6763c79d827a1f4ced6f7cf
Kyle <kyle.fetterhoff@gmail.com>
2019-12-20 20:22:42 -0500

    Add ReduxDevTools support for front-end

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import allReducers from "./reducers/index";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialState = {};
 
@@ -9,7 +10,7 @@ const middleware = [thunk];
 const store = createStore(
   allReducers,
   initialState,
-  applyMiddleware(...middleware)
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
